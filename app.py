2366b7e105634c9c5440dbf057e7d1ccbd970b77
Aswin <aswinkj2006@gmail.com>
2025-08-10 13:01:36 +0530

    image model error

--- app.py
+++ app.py
@@ -45,13 +45,12 @@ if st.button("Use Sample Image"):
 
 if uploaded_file:
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Uploaded Image", use_column_width=True)
-
     img_resized = image.resize((224, 224))
     img_array = np.array(img_resized) / 255.0
-    img_array = np.expand_dims(img_array, axis=0)
+    img_array = np.expand_dims(img_array, axis=0)  # shape (1, 224, 224, 3)
 
-    predictions = model.predict(img_array)[0]
+    predictions = model.predict(img_array)[0]  # model expects 1 input
+    st.image(image, caption="Uploaded Image", use_column_width=True)
     top_indices = predictions.argsort()[-4:][::-1]
 
     st.markdown("## 🏆 Prediction Result")
